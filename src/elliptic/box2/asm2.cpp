fc9078bc426613b97f700c93c26298d5d5c35114
Thilina Ratnayaka <thilinarmtb@gmail.com>
2026-07-07 16:24:06 +0000

    Setup coarse basis functions

--- src/elliptic/box2/asm2.cpp
+++ src/elliptic/box2/asm2.cpp
@@ -5,13 +5,15 @@
 
 struct asm2 {
   /* User input */
-  uint nd, nv;
+  uint ne, nd, nv;
   uint nbx, nby, nbz;
   double tol;
   /* Structured grid */
   double mnmx[3][2];
   double dx[3];
-  double *xyz;
+  ulong *lelem_to_gbox;
+  uint *gbox_to_lbox;
+  double *phi;
 };
 
 #define IDX3(E, V, D) (nv * nd * E + nd * V + D)
@@ -21,10 +23,13 @@ struct asm2 {
  *
  *  Global extremes of the domain.
  *--------------------------------------------------------------------*/
-void domain_size(double *min, double *max, const uint ne, const uint nd,
-                 const uint nv, const double *const xyz,
-                 const struct comm *const c) {
-  for (uint d = 0; d < nd; d++) min[d] = DBL_MAX, max[d] = DBL_MIN;
+static void domain_size(struct asm2 *const data, const double *const xyz,
+                        const struct comm *const c) {
+  const uint ne = data->ne, nd = data->nd, nv = data->nv;
+  assert(nd <= 3);
+
+  double min[3], max[3], wrk[3];
+  for (uint d = 0; d < nd; d++) min[d] = DBL_MAX, max[d] = -DBL_MAX;
 
   for (uint e = 0; e < ne; e++) {
     for (uint v = 0; v < nv; v++) {
@@ -35,10 +40,13 @@ void domain_size(double *min, double *max, const uint ne, const uint nd,
     }
   }
 
-  double wrk[3];
-  assert(nd <= 3);
   comm_allreduce(c, gs_double, gs_min, min, 3, &wrk);
   comm_allreduce(c, gs_double, gs_max, max, 3, &wrk);
+
+  for (uint d = 0; d < nd; d++) {
+    data->mnmx[d][0] = min[d];
+    data->mnmx[d][1] = max[d];
+  }
 }
 
 /*----------------------------------------------------------------------
@@ -47,14 +55,13 @@ void domain_size(double *min, double *max, const uint ne, const uint nd,
  *  Size the structured box grid from the global domain extent and fill
  *  the box vertex coordinate table; zero the per-box matrices.
  *--------------------------------------------------------------------*/
-static void set_crs_box_dims(struct asm2 *data, const double *const min,
-                             const double *const max) {
+static void set_crs_box_dims(struct asm2 *data) {
   assert(data->nd == 3 && "Only 3D meshes are supported");
 
+  const double xmax = data->mnmx[0][1], xmin = data->mnmx[0][0];
+  const double ymax = data->mnmx[1][1], ymin = data->mnmx[1][0];
+  const double zmax = data->mnmx[2][1], zmin = data->mnmx[2][0];
   const double tol = data->tol;
-  double xmax = max[0], xmin = min[0];
-  double ymax = max[1], ymin = min[1];
-  double zmax = max[2], zmin = min[2];
 
   double dx = xmax - xmin;
   double x0 = xmin - tol * dx;
@@ -78,30 +85,90 @@ static void set_crs_box_dims(struct asm2 *data, const double *const min,
   dx = data->dx[0] = (x1 - x0) / data->nbx;
   dy = data->dx[1] = (y1 - y0) / data->nby;
   dz = data->dx[2] = (z1 - z0) / data->nbz;
+}
 
-  const uint nbox = data->nbx * data->nby * data->nbz;
-  const uint nd = data->nd;
+/*----------------------------------------------------------------------
+ * find_crs_interp
+ *
+ * Find the coarse box to which each spectral element belongs to and
+ * then find the prolongation operator from local elements to coarse
+ * boxes.
+ *--------------------------------------------------------------------*/
+void find_crs_interp(struct asm2 *const data, const double *const xyz,
+                     const double *const centroid) {
+  const uint ne = data->ne;
   const uint nv = data->nv;
-  data->xyz = tcalloc(double, nbox * nv * nd);
-  uint i, j, k, l = 0, ii, kk, jj, ll;
-  for (k = 0; k < data->nbz; k++) {
-    for (j = 0; j < data->nby; j++) {
-      for (i = 0; i < data->nbx; i++) {
-        ll = 0;
-        for (kk = 0; kk <= 1; kk++) {
-          for (jj = 0; jj <= 1; jj++) {
-            for (ii = 0; ii <= 1; ii++) {
-              data->xyz[IDX3(l, ll, 0)] = x0 + dx * (i + ii);
-              data->xyz[IDX3(l, ll, 1)] = y0 + dy * (j + jj);
-              data->xyz[IDX3(l, ll, 2)] = z0 + dz * (k + kk);
-              ll = ll + 1;
-            }
-          }
-        }
-        l = l + 1;
-      }
+  const uint nd = data->nd;
+  const uint nbx = data->nbx;
+  const uint nby = data->nby;
+  const uint nbz = data->nbz;
+  const ulong nbxyz = (ulong)nbx * (ulong)nby * (ulong)nbz;
+
+  data->lelem_to_gbox = tcalloc(ulong, ne);
+  data->phi = tcalloc(double, ne * nv * nv);
+  data->gbox_to_lbox = tcalloc(uint, nbxyz);
+
+  const double x0 = data->mnmx[0][0], dx = data->dx[0];
+  const double y0 = data->mnmx[1][0], dy = data->dx[1];
+  const double z0 = data->mnmx[2][0], dz = data->dx[2];
+  uint nbox = 0;
+  for (uint e = 0; e < ne; e++) {
+    double x = centroid[e * nd + 0];
+    double y = centroid[e * nd + 1];
+    double z = centroid[e * nd + 2];
+
+    ulong ig = (ulong)(fabs(x - x0) / dx);
+    ulong jg = (ulong)(fabs(y - y0) / dy);
+    ulong kg = (ulong)(fabs(z - z0) / dz);
+
+    if (ig >= nbx) ig = nbx - 1;
+    if (jg >= nby) jg = nby - 1;
+    if (kg >= nbz) kg = nbz - 1;
+    ulong bg = ig + nbx * jg + nbx * nby * kg;
+
+    data->lelem_to_gbox[e] = bg;
+    if (data->gbox_to_lbox[bg] == 0) data->gbox_to_lbox[bg] = ++nbox;
+
+    double xm = x0 + ig * dx, xM = x0 + (ig + 1) * dx;
+    double ym = y0 + jg * dy, yM = y0 + (jg + 1) * dy;
+    double zm = z0 + kg * dz, zM = z0 + (kg + 1) * dz;
+
+    for (uint v = 0; v < nv; v++) {
+      // calculate the reference coordinates.
+      double rb = -1.0 + 2 * (xyz[IDX3(e, v, 0)] - xm) / (xM - xm);
+      double sb = -1.0 + 2 * (xyz[IDX3(e, v, 1)] - ym) / (yM - ym);
+      double tb = -1.0 + 2 * (xyz[IDX3(e, v, 2)] - zm) / (zM - zm);
+
+      // evaluate the hat functions at the reference coordinates.
+      double phi0_r = 0.5 * (1 - rb);
+      double phi1_r = 0.5 * (1 + rb);
+      double phi0_s = 0.5 * (1 - sb);
+      double phi1_s = 0.5 * (1 + sb);
+      double phi0_t = 0.5 * (1 - tb);
+      double phi1_t = 0.5 * (1 + tb);
+
+      data->phi[e * nv * nv + v * nv + 0] = phi0_r * phi0_s * phi0_t;
+      data->phi[e * nv * nv + v * nv + 1] = phi1_r * phi0_s * phi0_t;
+      data->phi[e * nv * nv + v * nv + 2] = phi0_r * phi1_s * phi0_t;
+      data->phi[e * nv * nv + v * nv + 3] = phi1_r * phi1_s * phi0_t;
+      data->phi[e * nv * nv + v * nv + 4] = phi0_r * phi0_s * phi1_t;
+      data->phi[e * nv * nv + v * nv + 5] = phi1_r * phi0_s * phi1_t;
+      data->phi[e * nv * nv + v * nv + 6] = phi0_r * phi1_s * phi1_t;
+      data->phi[e * nv * nv + v * nv + 7] = phi1_r * phi1_s * phi1_t;
     }
   }
+}
+
+/*----------------------------------------------------------------------
+ * free_asm2_data
+ *
+ * Free the arrays allocated inside the asm2 struct.
+ *--------------------------------------------------------------------*/
+static void free_asm2_data(struct asm2 *const data) {
+  if (data == NULL) return;
+  free(data->lelem_to_gbox), data->lelem_to_gbox = NULL;
+  free(data->gbox_to_lbox), data->gbox_to_lbox = NULL;
+  free(data->phi), data->phi = NULL;
 }
 
 struct xxt *crs_asm2_setup(const uint ne, const uint nd, const uint nv,
@@ -110,19 +177,28 @@ struct xxt *crs_asm2_setup(const uint ne, const uint nd, const uint nv,
                            const uint nby, const uint nbz,
                            const struct comm *const c) {
   struct asm2 data;
-  data.nd = nd, data.nv = nv, data.nbx = nbx, data.nby = nby, data.nbz = nbz;
+  data.ne = ne, data.nd = nd, data.nv = nv;
+  data.nbx = nbx, data.nby = nby, data.nbz = nbz;
   data.tol = 1e-2;
 
-  double min[3], max[3];
-  domain_size(min, max, ne, nd, nv, xyz, c);
-  set_crs_box_dims(&data, min, max);
+  domain_size(&data, xyz, c);
 
+  set_crs_box_dims(&data);
   if (c->id == 0) {
-    printf("%g %g box domain x %d\n", min[0], max[0], data.nbx);
-    printf("%g %g box domain y %d\n", min[1], max[1], data.nby);
-    printf("%g %g box domain z %d\n", min[2], max[2], data.nbz);
+    printf("%g %g box domain x %d\n", data.mnmx[0][0], data.mnmx[0][1],
+           data.nbx);
+    printf("%g %g box domain y %d\n", data.mnmx[1][0], data.mnmx[1][1],
+           data.nby);
+    printf("%g %g box domain z %d\n", data.mnmx[2][0], data.mnmx[2][1],
+           data.nbz);
     printf("%g %g %g box dxdydz\n", data.dx[0], data.dx[1], data.dx[2]);
   }
+
+  find_crs_interp(&data, xyz, centroid);
+
+  free_asm2_data(&data);
+
+  return 0;
 }
 
 void crs_asm2_solve(occa::memory &o_x, struct xxt *xxt, occa::memory &o_rhs) {}
